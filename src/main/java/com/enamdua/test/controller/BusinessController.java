5801e2b6a957a67f84b2cade4c4b00434a9108fb
eulbyvan <work.issdq@gmail.com>
2023-09-02 21:08:17 +0700

    Change endpoint according to the requirement docs

--- src/main/java/com/enamdua/test/controller/BusinessController.java
+++ src/main/java/com/enamdua/test/controller/BusinessController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v3/businesses")
+@RequestMapping("/business")
 public class BusinessController {
 
     @Autowired
